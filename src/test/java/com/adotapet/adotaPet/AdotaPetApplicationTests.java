7e8aee7e3766b232d57bb6d25724e772088ffbf4
ArthurPeruzzo <arthur.kohl@viasoft.com.br>
2024-08-04 10:15:14 -0300

    initial commit

---
+++ src/test/java/com/adotapet/adotaPet/AdotaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adotapet.adotaPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
